dfefaa0c8b8f88bb065dd6a0972a4036edf56494
Suraj Mishra <suraj.mishra@iot83.com>
2025-08-02 17:27:49 +0530

    loggingframework lld added

---
+++ src/main/java/org/example/lld_interview_problems/loggingframework/appender/LogAppender.java
@@ -0,0 +1,8 @@
+package org.example.lld_interview_problems.loggingframework.appender;
+
+
+import org.example.lld_interview_problems.loggingframework.LogMassage;
+
+public interface LogAppender {
+    void append(LogMassage logMassage);
+}
---
+++ src/main/java/org/example/lld_interview_problems/loggingframework/formatter/LogFormatter.java
@@ -0,0 +1,8 @@
+package org.example.lld_interview_problems.loggingframework.formatter;
+
+
+import org.example.lld_interview_problems.loggingframework.LogMassage;
+
+public interface LogFormatter {
+    String formate(LogMassage massage);
+}
